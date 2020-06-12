ea0e7427cf85af52ce55ef3822b9b88d93a79041
Subhankar Bag <subhankar.codelogicx@gmail.com>
2020-06-12 18:07:45 +0530

    ~ Initial Commit

---
+++ app/src/test/java/com/securnyx360/projectember/ExampleUnitTest.kt
@@ -0,0 +1,17 @@
+package com.securnyx360.projectember
+
+import org.junit.Test
+
+import org.junit.Assert.*
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+class ExampleUnitTest {
+    @Test
+    fun addition_isCorrect() {
+        assertEquals(4, 2 + 2)
+    }
+}
